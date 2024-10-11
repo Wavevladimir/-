c52083669c6137caa42d0a4462dd957435eaa6d8
Wavevladimir <vikirov43@mail.ru>
2024-10-11 20:23:58 +0300

    magic method

---
+++ magic method.py
@@ -0,0 +1,10 @@
+int_num = 50
+str_val = 'abc'
+print(int_num * str_val)
+
+#print(int_num * float_num)
+#print(int_num.__mul__(float_num))
+#NotImplomented
+
+#print(float_num.__rmul__(int_num))
+#375.0
