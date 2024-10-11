2f9a38eaddcdf9d72b25882921a4bafbac93f9a4
Wavevladimir <vikirov43@mail.ru>
2024-10-11 12:54:32 +0300

    float

---
+++ float.py
@@ -0,0 +1,11 @@
+average_price = 28.75
+price = int(average_price)
+
+print(price)
+print(type(price))
+
+str_temperature = '14.5'
+temperature = float(str_temperature)
+
+print(temperature)
+print(type(temperature)) 
