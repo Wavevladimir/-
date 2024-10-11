e65bfab6f52361c720848ad5b77c20f751f6eed4
Wavevladimir <vikirov43@mail.ru>
2024-10-11 13:38:31 +0300

    bool

---
+++ bool.py
@@ -0,0 +1,19 @@
+db_is_available = False
+
+print(db_is_available)
+print(type(db_is_available))
+
+db_is_available = True
+
+print(db_is_available)
+
+print(bool(10))
+print(bool('abc'))
+print(bool([]))
+print(bool([1, 2]))
+print(bool(None))
+
+print(100 > 10)
+print('Long string' > 'Long')
+print([1] == [1])
+print({'a': 3} == {'a': 3})
--- complex.py
+++ complex.py
@@ -2,3 +2,4 @@ complex_a = 10 + 7j
 complex_b = 3 + 3j
 
 print(complex_a - complex_b)
+(10 + 7j)(3 + 3j) = 30 + 30j + 21j + 21j^2 = 9 + 51j
