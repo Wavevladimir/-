f28f5435047191bde6c720cc8ffcfa410354db84
Wavevladimir <vikirov43@mail.ru>
2024-10-16 14:09:24 +0300

    method dict

---
+++ method dict.py
@@ -0,0 +1,21 @@
+happy_smiles = []
+
+happy_smiles.append('=)')
+happy_smiles.append('=))')
+happy_smiles.append('=)))')
+happy_smiles.append('=))))')
+
+print(happy_smiles)
+
+inputs = [True, 123, 'hi']
+inputs.pop(1)
+print(inputs)
+del_element = inputs.pop(1)
+print(del_element)
+
+posts_ids = [245, 456, 721, 123, 111]
+posts_ids.sort()
+print(posts_ids)
+
+posts_ids.sort(reverse=True)
+print(posts_ids)
