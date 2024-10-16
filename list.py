873eab6f0a2c0afbfb538d5ca3b330942bd86c65
Wavevladimir <vikirov43@mail.ru>
2024-10-16 13:14:48 +0300

    list

---
+++ list.py
@@ -0,0 +1,20 @@
+empty_list = []
+print(len(empty_list))
+
+my_fruits = ['apple', 'banana', 'lime']
+print(len(my_fruits))
+
+posts_ids = [151, 345, 986, 204]
+print(len(posts_ids))
+
+print(posts_ids[0])
+print(posts_ids[1])
+print(posts_ids[-1])
+
+posts_ids[0] = 1256
+posts_ids[2] = 4567
+print(posts_ids)
+
+user_inputs = [True, 'hi', 1259,]
+del user_inputs[1]
+print(user_inputs)
