f3e47d606f236e10abf8fa6cd0e57847ac8b2606
Wavevladimir <vikirov43@mail.ru>
2024-10-16 13:52:22 +0300

    dict

---
+++ dictionary.py
@@ -0,0 +1,21 @@
+users = [
+    {
+        'user_id': 134,
+        'user_name': 'Alice'
+    },
+    {
+        'user_id': 834,
+        'user_name': 'Bob'
+    }
+]
+
+print(len(users))
+
+print(users[1]['user_name'])
+
+my_fruit = 'apple'
+new_fruit = 'banana'
+other_fruit = 'lime'
+
+all_fruit = [my_fruit, new_fruit, other_fruit]
+print(all_fruit)
