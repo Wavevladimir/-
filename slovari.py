1c8a34de3f889544c12a047ad9957beef4a20739
Wavevladimir <vikirov43@mail.ru>
2024-11-17 17:28:00 +0300

    slovari

--- slovari.py
+++ slovari.py
@@ -1,7 +1,10 @@
 my_mothorbike = {
     'brand': 'Ducati',
-    'price': 25000, 
-    'engine_vol': 1.2
+    'engine_vol': 1.2,
+    'price_info': {
+        'price': 20000,
+        'is_availble': True
+    }
 }
 
 other_mothobike = {
@@ -11,3 +14,17 @@ other_mothobike = {
 }
 
 print(my_mothorbike == other_mothobike)
+
+print(my_mothorbike['brand'])
+
+my_mothorbike['price'] = 7000
+print(my_mothorbike)
+
+my_mothorbike['is_new'] = True
+
+del my_mothorbike['engine_vol']
+
+key_name = 'brand'
+
+other_mothobike[key_name] = 'BMW'
+print(other_mothobike)
