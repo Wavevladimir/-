a74f93202ac8fc8907495492ec7069764b2562cf
Wavevladimir <vikirov43@mail.ru>
2024-11-17 17:04:58 +0300

    slovari

---
+++ slovari.py
@@ -0,0 +1,13 @@
+my_mothorbike = {
+    'brand': 'Ducati',
+    'price': 25000, 
+    'engine_vol': 1.2
+}
+
+other_mothobike = {
+    'price': 25000,
+    'engine_vol': 1.2,
+    'brand': 'Ducati'
+}
+
+print(my_mothorbike == other_mothobike)
