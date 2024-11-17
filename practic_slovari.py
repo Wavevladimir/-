d416ab9c3d99af9ce510862bd7f12f3faf135a7d
Wavevladimir <vikirov43@mail.ru>
2024-11-17 22:53:22 +0300

    copy slovari

---
+++ practic_slovari.py
@@ -0,0 +1,20 @@
+my_disk = {}
+
+print(id(my_disk))
+print(type(my_disk))
+
+my_disk['brand'] = 'Samsung'
+my_disk['price'] = 80
+
+#print(my_disk)
+#print(id(my_disk))
+
+print(list(my_disk.keys()))
+
+print(my_disk.get('type'))
+
+new_disk = my_disk.copy()
+
+new_disk['type'] = 'ssd'
+print(my_disk)
+print(new_disk)
