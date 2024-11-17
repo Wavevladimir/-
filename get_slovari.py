55092b0c9b7f6a43951d9e3785d5584fc2285f77
Wavevladimir <vikirov43@mail.ru>
2024-11-17 17:54:43 +0300

    get slovari

---
+++ get_slovari.py
@@ -0,0 +1,9 @@
+my_mothobike = {
+    'price': 25000,
+    'engine_vol': 1.2,
+    'brand': 'Ducati'
+}   
+
+print(my_mothobike.get('brand'))
+print(my_mothobike.get('price'))
+print(my_mothobike.get('qty', 0))
