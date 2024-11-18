7583056696d90d6c13cff743f2364beec6a2e2a3
Wavevladimir <vikirov43@mail.ru>
2024-11-18 22:42:59 +0300

    tuple

---
+++ tuple.py
@@ -0,0 +1,15 @@
+my_fruits = ('apple', 'banana', 'lime')
+posts_ids = (151, 245, 762, 167)
+user_inputs = (True, 'hi', '=)', 10.5)
+
+other_fruits = ('banana', 'lime', 'apple')
+
+print(my_fruits == other_fruits)
+
+print(len(posts_ids))
+print(posts_ids[0])
+print(posts_ids[-1])
+
+my_nums = (10, 4, 8, 0)
+print(type(my_nums))
+my_nums[1] = 7
