ccc3782505a77b666a9b19d22b10a286f93e2cee
Wavevladimir <vikirov43@mail.ru>
2024-11-18 23:31:34 +0300

    tuple_method

---
+++ tuple_method.py
@@ -0,0 +1,14 @@
+#post_ids = (151, 245, 762, 245)
+#print(post_ids.count(245))
+
+#print(post_ids.index(245))
+
+posts_ids = (151, 245)
+
+posts_ids_list = list(posts_ids)
+posts_ids_list.append(351)
+
+print(posts_ids_list)
+
+posts_ids_tuple = tuple(posts_ids_list)
+print(posts_ids_tuple)
