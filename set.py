b18a929872fc2732a642f9ac8f309a5363670e39
Wavevladimir <vikirov43@mail.ru>
2024-11-19 22:49:11 +0300

    set

--- set.py
+++ set.py
@@ -7,3 +7,16 @@ print(my_fruits)
 print(type(my_fruits))
 print(my_fruits == other_fruits)
 print(len(my_fruits))
+
+#print(my_fruits[0]) IndexError
+#lists_set = {[1, 2], [5, 20]} Error unhshable type 
+
+my_set = {10, 10, 5, 15, 15}
+
+print(my_set)
+print(len(my_set))
+#del my_set[0] error
+
+set = set() 
+print(set)
+print(type(set))
