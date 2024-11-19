49d8169a2c5552a364fbcbb4110ba59f2091216c
Wavevladimir <vikirov43@mail.ru>
2024-11-19 22:31:02 +0300

    set

---
+++ set.py
@@ -0,0 +1,9 @@
+my_fruits = {'apple', 'banana', 'lime'}
+other_fruits = {'banana', 'apple', 'lime'}
+posts_ids = {123, 234, 567, 127}
+user_inputs = {True, 'hi!', '=)', 10.5}
+
+print(my_fruits)
+print(type(my_fruits))
+print(my_fruits == other_fruits)
+print(len(my_fruits))
