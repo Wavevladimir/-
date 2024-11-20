93acc88101208fd7a8540561cff4b502f23b95ae
Wavevladimir <vikirov43@mail.ru>
2024-11-20 12:42:44 +0300

    range_method

---
+++ range_practic.py
@@ -0,0 +1,22 @@
+#my_range = range(5)
+
+#print(my_range)
+#print(type(my_range))
+#print(my_range[0])
+
+#for n in my_range:
+ #   print(n)
+
+#for n in range(12, 25, 5):
+#    print(n)
+
+#print(list(range(12, 25, 5)))
+#print(tuple(range(12, 25, 5)))
+#print(set(range(12, 25, 5)))
+
+my_range = range(10, 30, 3)
+#print(dir(my_range))
+print(my_range.start)
+print(my_range.stop)
+print(my_range.step)
+print(my_range.index(19))
