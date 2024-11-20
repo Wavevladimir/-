a294de8da9dc1d63a260d765c60f61d6e2888b9f
Wavevladimir <vikirov43@mail.ru>
2024-11-20 11:25:49 +0300

    method_set_practic

---
+++ method_set_practic.py
@@ -0,0 +1,18 @@
+my_set = {'abc', 'd', 'f', 'y'}
+other_set = {'a', 'f', 'd'}
+print(my_set.intersection(other_set))
+print(other_set.intersection(my_set))
+
+#print(my_set.union(other_set)) обьединение наборов
+
+#print(my_set.issubset(other_set)) подмножество
+#print(my_set.discard('d')) удаление элемента d
+
+print(my_set == other_set)
+print(my_set.difference(other_set))
+
+copied_set = my_set.copy()
+my_set.add('t')
+copied_set.add('l')
+print(my_set)
+print(copied_set)
