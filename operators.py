84444610ae4cb06502ec51ebc217454e697b89b9
Wavevladimir <vikirov43@mail.ru>
2024-11-26 23:08:24 +0300

    operators

--- operators.py
+++ operators.py
@@ -14,3 +14,21 @@ print(a.__eq__(b))
 
 print(id(d) == id(f))
 print(hex(id(d)))
+
+my_num = 10
+print(+my_num)
+
+my_bool = False
+print(+my_bool)
+
+mu_numb = 10
+print(not my_num)
+
+my_car = {
+    'brand': 'Toyota',
+    'price': 10_000
+}
+
+print('brand' in my_car)
+print('year' in my_car)
+print('year' not in my_car)
