c741d7e52f8a6fd494f918661f38b894a4710e71
Wavevladimir <vikirov43@mail.ru>
2024-11-28 22:22:12 +0300

    False

---
+++ false.py
@@ -0,0 +1,22 @@
+#False
+#int 0
+#float 0.0
+#complex 0j
+
+print(bool(0))
+print(bool(0.0))
+print(bool(0j))
+print(bool(None))
+
+print(bool([]))
+print(bool({}))
+print(bool(set()))
+print(bool(range(0)))
+print(bool(()))
+print(bool(''))
+
+print(not not {})
+
+my_list = [1, 2]
+if len(my_list) > 0:
+    print("Last has elements")
