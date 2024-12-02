52d7718cc1e6831d08ac60ba220ae147373639af
Wavevladimir <vikirov43@mail.ru>
2024-12-02 20:30:24 +0300

    f_strings

---
+++ f_strings.py
@@ -0,0 +1,20 @@
+'hello ' + 'python'
+
+a = 'Hello'
+b = 'World'
+add = a + ' ' + b
+print(add)
+
+hello = 'Hello'
+world = 'World'
+
+greeting = f"{hello} {world}"
+print(greeting)
+
+my_name = 'Vova'
+my_hobby = 'running'
+time = 8
+
+#info = my_name + ' likes ' + my_hobby + ' at' + str(time) + ' o\'clock'
+info = f"{my_name} likes {my_hobby} at {str(time)} o\'clock"
+print(info)
