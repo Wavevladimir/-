deeea1747a90fd192733a94630df7fb6d9fea0a9
Wavevladimir <vikirov43@mail.ru>
2024-12-10 18:38:36 +0300

    practic for in

--- practic_short_for_in.py
+++ practic_short_for_in.py
@@ -9,3 +9,17 @@ my_scores = [10, 7, 14]
 scores = {index: elem * 2 for index, elem in enumerate(my_scores)}
 
 print(scores)
+
+my_motorbike = {
+    'brand': 'bmw',
+    'country': 'germany',
+    'owner': 'vova'
+}
+
+bike = {k: v.upper() for k , v in my_motorbike.items()}
+print(bike)
+
+my_moto = ['Ducati', 'Italy', 'Vi']
+
+motobike = [el for el in my_moto if len(el) > 3]
+print(motobike)
