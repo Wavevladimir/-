57a0f2ca169b190b283b7851e249960d5829c1ed
Wavevladimir <vikirov43@mail.ru>
2024-12-11 17:45:08 +0300

    __init__

--- classes_and_object.py
+++ classes_and_object.py
@@ -15,3 +15,16 @@ Car.move(my_car)
 print(my_car == my_second_car)
 my_car.move()
 my_car.stop()
+
+class Comment:
+    def __init__(self, text):
+        self.text = text
+        self.votes_qty = 0
+    def upvote(self):
+        self.votes_qty += 1 
+first_comment = Comment("First comment")
+print(first_comment.text)
+print(first_comment.upvote)
+print(first_comment.__dict__)
+print(dir(first_comment))
+print(type(first_comment))
