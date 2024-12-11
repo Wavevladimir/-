c2a7fb107e627dddab653e2543004cce54d967d9
Wavevladimir <vikirov43@mail.ru>
2024-12-11 12:46:39 +0300

    practic

--- classes_and_object.py
+++ classes_and_object.py
@@ -7,3 +7,11 @@ class Car:
 my_car = Car()
 print(type(my_car))
 print(isinstance(my_car, Car))
+print(isinstance(my_car, object))
+
+my_second_car = Car()
+Car.move(my_car)
+
+print(my_car == my_second_car)
+my_car.move()
+my_car.stop()
