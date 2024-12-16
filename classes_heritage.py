fc4a258db6e770183400b0dadc571b71067269dd
Wavevladimir <vikirov43@mail.ru>
2024-12-16 23:30:46 +0300

    heritage

--- classes_heritage.py
+++ classes_heritage.py
@@ -3,3 +3,8 @@ class   ExtendedList(list):
         print(f"List has {len(self)} elements")
 custom_list = ExtendedList([3, 5, 2])
 custom_list.print_list_info()
+custom_list.append(7)
+custom_list.print_list_info()
+
+print(ExtendedList.__dict__)
+print(list.__subclasses__())
