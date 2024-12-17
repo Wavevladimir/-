ff82762e22204a51bbc9d13e78232f29577dd3d0
Wavevladimir <vikirov43@mail.ru>
2024-12-17 23:51:52 +0300

    wrapper fn

--- decorators.py
+++ decorators.py
@@ -1,13 +1,16 @@
 def decoration_fuction(original_fn):
-    def wrapper_fn():
+    def wrapper_fn(a, b):
         print("Executed before function")
-        result = original_fn()
+        result = original_fn(a, b)
+        print("Executed after function")
 
         return result
     return wrapper_fn
 
 @decoration_fuction
-def my_function():
+def my_function(a, b):
     print("This is my function!")
+    return (a , b)
 
-my_function()
+result = my_function(100, 50)
+print(result)
