388b2b00f1d9cca4f4d6aaa363c469e18a7cede4
Wavevladimir <vikirov43@mail.ru>
2024-12-20 23:12:42 +0300

    module practic

---
+++ module_practic_one.py
@@ -0,0 +1,4 @@
+def print_sum(a, b):
+    print(a + b)
+
+my_name = 'Vova'
---
+++ module_practic_two.py
@@ -0,0 +1,8 @@
+import module_practic_one as mod_one
+
+print(mod_one)
+print(type(mod_one))
+print(dir(mod_one))
+
+print(mod_one.my_name)
+mod_one.print_sum(5, 3)
