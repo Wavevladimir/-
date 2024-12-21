0a40d14d379e39d8f92919f4372740dfd906d6f0
Wavevladimir <vikirov43@mail.ru>
2024-12-21 23:36:20 +0300

    main and name

--- module_practic_one.py
+++ module_practic_one.py
@@ -2,3 +2,6 @@ def print_sum(a, b):
     print(a + b)
 
 my_name = 'Vova'
+
+print(__name__)
+print(__name__ == '__main__')
--- module_practic_two.py
+++ module_practic_two.py
@@ -1,4 +1,7 @@
-import module_practic_one as mod_one
+import module_practic_one as mod_one, my_fn
+# from module_practic_one import my_name, print_sum as sum
+# sum(5, 2)
+# print(my_name)
 
 print(mod_one)
 print(type(mod_one))
@@ -6,3 +9,7 @@ print(dir(mod_one))
 
 print(mod_one.my_name)
 mod_one.print_sum(5, 3)
+
+print(__name__)
+print(__name__ == '__main__')
+
