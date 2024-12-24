38527bb67f46a9e106d8836028135a627f15f08d
Wavevladimir <vikirov43@mail.ru>
2024-12-24 23:40:49 +0300

    json import

---
+++ json.py
@@ -0,0 +1,9 @@
+import json
+json_str = '{"id":235, "brand": "Nike", "qty":84, "status": {"isForSale":true}}'
+sneakers = json.loads(json_str)
+
+print(type(sneakers))
+
+print(sneakers['brand'])
+print(sneakers['qty'])
+print(sneakers['status']['isForSale'])
