ffb28b52c482e6738746f7411630ebede1e00124
Wavevladimir <vikirov43@mail.ru>
2025-01-04 19:06:40 +0300

    zip unpacking

--- zip_practic.py
+++ zip_practic.py
@@ -1,16 +1,21 @@
 from zipfile import ZipFile
 from pathlib import Path
 
+with ZipFile('my-files.zip', mode='r') as my_zip_file:
+    my_zip_file.extractall('my-files-unzipped')
+    print(my_zip_file.infolist())
+    print(my_zip_file.filename)
+
 # Path('my-files').mkdir()
 
 # with open('my-files/first.txt', 'w') as my_file:
 #     my_file.write("This is first file")
     
 # with open('my-files/second.txt', 'w') as my_file:
-#     my_file.write("This is second file")
+#     my_file.write("This is second file") создание файлов которые мы запакуем
 
-with ZipFile('my-files.zip', mode='w') as my_zip_file:
-    # print(my_zip_file)
-    for file in Path('my-files').iterdir():
-        print(file)
-        my_zip_file.write(file)
+# with ZipFile('my-files.zip', mode='w') as my_zip_file:
+#     # print(my_zip_file)
+#     for file in Path('my-files').iterdir():
+#         print(file)
+#         my_zip_file.write(file) создания зип файла 
