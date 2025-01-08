01ef567d7d20d2da6b0f001636430bc9e8dc33f3
Wavevladimir <vikirov43@mail.ru>
2025-01-08 11:12:05 +0300

    re_module

--- regular_expressions_module.py
+++ regular_expressions_module.py
@@ -1,5 +1,12 @@
 import re
-my_string = "My name is Vova"
-res = re.search('Vova', my_string)
+my_string = "My name is Vova."
+res = re.search('V..a', my_string)
 print(res)
 print(type(res))
+print(res.start())
+print(res.end())
+print(r"V..a\n.$")
+
+my_pattern = re.compile(r'^My.*\.$')
+print(my_pattern)
+print(my_pattern.match(my_string))
