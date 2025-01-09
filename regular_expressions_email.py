c584891fca5b88ec01a0ac0bf2676a39c3a5a524
Wavevladimir <vikirov43@mail.ru>
2025-01-09 23:52:50 +0300

    check email

--- regular_expressions_email.py
+++ regular_expressions_email.py
@@ -1,6 +1,18 @@
 import re
 
-email_regexp = r"^[a-zA-Z0-9_.]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-.]+$"
 
-email_check_pattern = re.compile(email_regexp)
-print(email_check_pattern.fullmatch('bs@gmail.com'))
+def check_email(email):
+    email_regexp = r"^[a-zA-Z0-9_.]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-.]+$"
+    email_check_pattern = re.compile(email_regexp)
+    validation_result = "Valid" if email_check_pattern.fullmatch(email) else "Not valid"
+    return (email, validation_result)
+    
+# Valid
+print(check_email('bs@gmail.com'))
+print(check_email('b_s@gmail.com'))
+print(check_email('b.s@gmail.com'))
+print(check_email('b.s@sub.gmail.com'))
+# Invalid
+print(check_email('@gmail.com'))
+print(check_email('bsgmail.com'))
+print(check_email('bs@'))
