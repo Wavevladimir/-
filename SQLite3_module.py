37e946c1dbf04d5d52e49b52580e0c590f019905
Wavevladimir <vikirov43@mail.ru>
2025-01-13 22:31:20 +0300

    reading at the table sqlite3

--- SQLite3_module.py
+++ SQLite3_module.py
@@ -1,17 +1,25 @@
 import sqlite3 
 DB_NAME = "sqlite_db.db"
 
-courses = [
-    (351, "JavaScript course", 451, 100),
-    (614, "C++ course", 161, 10),
-    (721, "Java full course", 100, 35)
-]
-
 with sqlite3.connect(DB_NAME) as sqlite_conn:
-    sql_request = "INSERT INTO courses VALUES(?, ?, ?, ?)"
-    for course in courses:
-        sqlite_conn.execute(sql_request, course)
-    sqlite_conn.commit()
+    sql_request = "SELECT * FROM courses WHERE reviews_qty>=30"
+    sql_cursor = sqlite_conn.execute(sql_request)
+    # for record in sql_cursor:
+    #     print(record[1])
+    records = sql_cursor.fetchall()
+    print(records)
+# add records to the courses table
+# courses = [
+#     (351, "JavaScript course", 451, 100),
+#     (614, "C++ course", 161, 10),
+#     (721, "Java full course", 100, 35)
+# ]
+
+# with sqlite3.connect(DB_NAME) as sqlite_conn:
+#     sql_request = "INSERT INTO courses VALUES(?, ?, ?, ?)"
+#     for course in courses:
+#         sqlite_conn.execute(sql_request, course)
+#     sqlite_conn.commit()
 
 # create file db
 # with sqlite3.connect(DB_NAME) as sqlite_conn: 
