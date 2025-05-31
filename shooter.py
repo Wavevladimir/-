57fc769365832352682547de249d5dce1bbaeaa4
Wavevladimir <vikirov43@mail.ru>
2025-05-31 11:48:49 +0300

    rect going

--- shooter.py
+++ shooter.py
@@ -5,19 +5,34 @@ import sys
 
 
 pygame.init()
-screen_width = 800
-screen_height = 600
+screen_width,screen_height = 800, 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("My Pygame")
+fill_color = (32, 52, 71)
 
+STEP = 10
+
+rect_width, rect_height = 100, 200
+rect_x = screen_width / 2 - rect_width / 2
+rect_y = screen_height / 2 - rect_height / 2
+rect_color = pygame.Color('lightyellow')
 while True:
     for event in pygame.event.get():
         print(event)
         if event.type == pygame.QUIT:
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                rect_y -= STEP
+            if event.key == pygame.K_DOWN:
+                rect_y += STEP
+            if event.key == pygame.K_LEFT:
+                rect_x -= STEP
+            if event.key == pygame.K_RIGHT:
+                rect_x += STEP
 
-    screen.fill((255, 255, 255))
-    pygame.draw.rect(screen, (0, 255, 0), (0, 0, 100, 200))
+    screen.fill(fill_color)
+    pygame.draw.rect(screen, rect_color, (rect_x, rect_y, rect_width, rect_height))
     pygame.display.update()
 
