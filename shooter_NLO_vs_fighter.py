69e57deea62a9fdf5a7a1fa41600cf21ddc98c8e
Wavevladimir <vikirov43@mail.ru>
2025-06-03 23:06:29 +0300

    image in the center

--- shooter_NLO_vs_fighter.py
+++ shooter_NLO_vs_fighter.py
@@ -1,8 +1,10 @@
 import pygame
+import sys
 
 pygame.init()
 
 screen_width, screen_height = 800, 600
+screen_fill_color = (32, 52, 71)
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("Awesome Shooter Game")
@@ -10,3 +12,14 @@ pygame.display.set_caption("Awesome Shooter Game")
 fighter_image = pygame.image.load('images/fighter.png')
 
 fighter_width, fighter_heigh = fighter_image.get_size()
+fighter_x, fighter_y = screen_width / 2 - fighter_width / 2, screen_height - fighter_heigh
+
+while True: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    screen.fill(screen_fill_color)
+    screen.blit(fighter_image, (fighter_x, fighter_y))
+    
+    pygame.display.update()
