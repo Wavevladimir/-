799aba40ecc953c0be4b2f23c41a8fcedd6b26d0
Wavevladimir <vikirov43@mail.ru>
2025-06-07 23:30:27 +0300

    fighter is moving right and left

--- shooter_NLO_vs_fighter.py
+++ shooter_NLO_vs_fighter.py
@@ -9,7 +9,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("Awesome Shooter Game")
 
-FIGHTER_STEP = 20
+FIGHTER_STEP = 0.5
 fighter_image = pygame.image.load('images/fighter.png')
 fighter_width, fighter_heigh = fighter_image.get_size()
 fighter_x, fighter_y = screen_width / 2 - fighter_width / 2, screen_height - fighter_heigh
@@ -20,19 +20,22 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT and fighter_x >= FIGHTER_STEP:
+            if event.key == pygame.K_LEFT:
                 fighter_is_moving_left = True
-                fighter_x -= FIGHTER_STEP
-            if event.key == pygame.K_RIGHT and fighter_x <= screen_width - fighter_width - FIGHTER_STEP:
+            if event.key == pygame.K_RIGHT:
                 fighter_is_moving_right = True
-                fighter_x += FIGHTER_STEP
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 fighter_is_moving_left = False
             if event.key == pygame.K_RIGHT:
                 fighter_is_moving_right = False
 
-    
+
+    if fighter_is_moving_left and fighter_x >= FIGHTER_STEP:
+        fighter_x -= FIGHTER_STEP
+
+    if fighter_is_moving_right and fighter_x <= screen_width - fighter_width - FIGHTER_STEP:
+        fighter_x += FIGHTER_STEP
 
 
     screen.fill(screen_fill_color)
