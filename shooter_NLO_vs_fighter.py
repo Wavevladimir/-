8459a40898938f330628a3f6289c6c645ece2da3
Wavevladimir <vikirov43@mail.ru>
2025-06-09 20:59:30 +0300

    ball

--- shooter_NLO_vs_fighter.py
+++ shooter_NLO_vs_fighter.py
@@ -15,7 +15,12 @@ fighter_width, fighter_heigh = fighter_image.get_size()
 fighter_x, fighter_y = screen_width / 2 - fighter_width / 2, screen_height - fighter_heigh
 fighter_is_moving_left, fighter_is_moving_right = False, False
 
-rocket_image = pygame.image.load('images/rocket.png')
+BALL_STEP = 0.3
+ball_image = pygame.image.load('images/ball.png')
+ball_width, ball_height = ball_image.get_size()
+ball_x, ball_y = fighter_x + fighter_width / 2 - ball_width / 2, fighter_y - ball_height
+ball_was_fired = False
+
 
 while True:
     for event in pygame.event.get():
@@ -23,9 +28,13 @@ while True:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                fighter_is_moving_left = True
+                 fighter_is_moving_left = True
             if event.key == pygame.K_RIGHT:
                 fighter_is_moving_right = True
+            if event.key == pygame.K_SPACE:
+                ball_was_fired = True
+                ball_x = fighter_x + fighter_width / 2 - ball_width / 2
+                ball_y = fighter_y - ball_height
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 fighter_is_moving_left = False
@@ -39,8 +48,16 @@ while True:
     if fighter_is_moving_right and fighter_x <= screen_width - fighter_width - FIGHTER_STEP:
         fighter_x += FIGHTER_STEP
 
+    if ball_was_fired and ball_y + ball_height < 0:
+        ball_was_fired = False
+
+    if ball_was_fired:
+        ball_y -= BALL_STEP
 
     screen.fill(screen_fill_color)
     screen.blit(fighter_image, (fighter_x, fighter_y))
+
+    if ball_was_fired:
+        screen.blit(ball_image, (ball_x, ball_y))
 
     pygame.display.update()
