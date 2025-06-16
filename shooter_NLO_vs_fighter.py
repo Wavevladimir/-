60eff57dc277cede031eed4fb6166982000956dc
Wavevladimir <vikirov43@mail.ru>
2025-06-16 22:37:40 +0300

    alien speed

--- shooter_NLO_vs_fighter.py
+++ shooter_NLO_vs_fighter.py
@@ -12,7 +12,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("Awesome Shooter Game")
 
-FIGHTER_STEP = 0.5
+FIGHTER_STEP = 1
 fighter_image = pygame.image.load('images/fighter.png')
 fighter_width, fighter_heigh = fighter_image.get_size()
 fighter_x, fighter_y = screen_width / 2 - fighter_width / 2, screen_height - fighter_heigh
@@ -25,6 +25,7 @@ ball_x, ball_y = fighter_x + fighter_width / 2 - ball_width / 2, fighter_y - bal
 ball_was_fired = False
 
 ALIEN_STEP = 0.1
+alien_speed = ALIEN_STEP
 alien_image = pygame.image.load('images/alien.png')
 alien_width, alian_height = alien_image.get_size()
 alien_x, alien_y = randint(0,screen_width - alien_width), 0
@@ -56,7 +57,7 @@ while game_is_running:
     if fighter_is_moving_right and fighter_x <= screen_width - fighter_width - FIGHTER_STEP:
         fighter_x += FIGHTER_STEP
 
-    alien_y += ALIEN_STEP
+    alien_y += alien_speed
 
     if ball_was_fired and ball_y + ball_height < 0:
         ball_was_fired = False
@@ -79,6 +80,7 @@ while game_is_running:
     if ball_was_fired and alien_x < ball_x < alien_x + alien_width - ball_width and alien_y < ball_y < alien_y + alian_height - ball_height:
         ball_was_fired = False
         alien_x, alien_y = randint(0,screen_width - alien_width), 0
+        alien_speed += ALIEN_STEP / 2
 
 game_over_text = game_font.render("Game Over", True, 'white')
 game_over_rectangle = game_over_text.get_rect()
