2f3fa1dbfa5f049a047f814c52d9e9ff7068c70f
Wavevladimir <vikirov43@mail.ru>
2025-06-24 16:10:10 +0300

    git start two

--- Git/Git_start.py
+++ Git/Git_start.py
@@ -1,3 +1,10 @@
 git config --global user.name "Wavevladimir"
 git config --list
-git config --global user.email vikirov43@mail.ru
+git config --global user.email vikirov43@mail.ru 
+
+git init создание нового git репозитория
+git branch -m <name> изменить название корневой ветки
+
+ls -la вводим данную команду в папке в которой инициализировали гит репозиторий и 
+видим скрытую папуц гит
+cd .git переходим в эту папку
