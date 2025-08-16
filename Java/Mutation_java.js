ffb5ad4780695893a6858b727a18f53014aea72d
Wavevladimir <vikirov43@mail.ru>
2025-08-16 21:40:22 +0300

    mutation at js

--- Java/Mutation_java.js
+++ Java/Mutation_java.js
@@ -18,3 +18,43 @@ const person2 = person
 
 person2.age = 26
 console.log(person.age)
+
+#Вариант 1 по избежанию мутаций
+
+const person = {
+    name: 'Bob',
+    age: 25
+}
+
+const person2 = Object.assign({}, person)
+
+person2.age = 26
+
+console.log(person2.age) // 26
+console.log(person.age) // 25
+
+#Вариант 2
+const person = {
+    name: 'Bob',
+    age: 25
+}
+
+const person2 = { ...person }
+person2.name = 'Alice'
+
+console.log(person2.name) // Alice
+console.log(person.name) // Bob
+
+#Вариант 3
+
+const person = {
+    name: 'Bob',
+    age: 23
+}
+
+const person2 = JSON.parse(JSON.stringify(person))
+
+person2.name = 'Alice'
+
+console.log(person2.name) // Alice
+console.log(person.name) // Bob
