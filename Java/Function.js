128acf0251172dddb34a6dd596aca9a410b6d1db
Wavevladimir <vikirov43@mail.ru>
2025-08-19 22:55:18 +0300

    assign

--- Java/Function.js
+++ Java/Function.js
@@ -37,3 +37,18 @@ function increasePersonAge(person) {
 
 increasePersonAge(personOne)
 console.log(personOne.age)
+
+const personOne = {
+    name: 'Vova',
+    age: 28
+}
+
+function increasePersonAge(person) {
+    const updatedPerson = Object.assign({}, person)
+    updatedPerson.age += 1
+    return updatedPerson
+}
+
+const updatedPersonOne = increasePersonAge(personOne)
+console.log(personOne.age)
+console.log(updatedPersonOne.age)
