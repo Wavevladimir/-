39b1e34a3c039cbe5fe688c585a10d37670aa571
Wavevladimir <vikirov43@mail.ru>
2025-08-20 10:45:37 +0300

    fn global and local

--- Java/global_local_window.js
+++ Java/global_local_window.js
@@ -12,3 +12,29 @@ myFn()
 
 console.log(a)
 console.log(b)
+
+const a = 5
+
+function myFn() {
+    function innerFn() {
+        console.log(a) //5
+    }
+    innerFn()
+}
+
+myFn()
+
+let a
+let b
+
+function myFn() {
+    let b
+    a = true
+    b = 10
+    console.log(a)
+}
+
+myFn()
+
+console.log(a)//true
+console.log(b)//undefiend
